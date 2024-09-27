59519898d7efc1b798fdf5d412bccd609d871b89
codemodel6 <codemodel6@gmail.com>
2024-09-27 17:07:03 +0900

    design : header 제작

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,30 @@
+const Header = () => {
+  return (
+    <div className="bg-background-dark fixed w-full h-24 z-99 flex justify-between items-center px-8">
+      <div className="flex justify-between items-center w-2/5 h-full">
+        <button className="text-primary-100 flex flex-col justify-center items-center bg-white h-1/2 w-2/5 text-lg">
+          molruco
+        </button>
+        <div className="bg-background-dark text-primary-100 flex flex-col justify-center items-center h-full w-3/5 text-xs">
+          몰루코 헤더
+        </div>
+      </div>
+      <div className="flex justify-around items-center w-2/5 h-full">
+        <button className="text-base cursor-pointer text-primary-100">
+          Intro
+        </button>
+        <button className="text-base cursor-pointer text-primary-100">
+          Career
+        </button>
+        <button className="text-base cursor-pointer text-primary-100">
+          Project
+        </button>
+        <button className="text-base cursor-pointer text-primary-100">
+          Education
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
     <div className="flex-col bg-red-500 w-full" style={{ height: "2000px" }}>
-      <header className="fixed bg-black w-full h-24"></header>
+      <Header />
       <div className="bg-yellow-300 w-full h-56"></div>
       <div className="bg-green-400 w-full h-56"></div>
       <div className="bg-blue-300 w-full h-56"></div>
