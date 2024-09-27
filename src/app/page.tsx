d666c52e81914f58f3015e7133472a9d4bb79e23
codemodel6 <codemodel6@gmail.com>
2024-09-27 17:18:33 +0900

    feat : Header 전역화 및 스크롤 침범 오류 해결중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
-import Header from "./components/Header";
-
 export default function Home() {
   return (
     <div className="flex-col bg-red-500 w-full" style={{ height: "2000px" }}>
-      <Header />
       <div className="bg-yellow-300 w-full h-56"></div>
       <div className="bg-green-400 w-full h-56"></div>
       <div className="bg-blue-300 w-full h-56"></div>
     </div>
+    //   <div
+    //   className="snap-y snap-mandatory overflow-scroll"
+    //   style={{ height: "calc(100vh - 6rem)" }}
+    // >
+    //   <section className="h-screen snap-start bg-blue-500 flex items-center justify-center">
+    //     <h1 className="text-white text-4xl">First Screen</h1>
+    //   </section>
+    //   <section className="h-screen snap-start bg-green-500 flex items-center justify-center">
+    //     <h1 className="text-white text-4xl">Second Screen</h1>
+    //   </section>
+    //   <section className="h-screen snap-start bg-red-500 flex items-center justify-center">
+    //     <h1 className="text-white text-4xl">Third Screen</h1>
+    //   </section>
+    //   <section className="h-screen snap-start bg-yellow-500 flex items-center justify-center">
+    //     <h1 className="text-white text-4xl">Fourth Screen</h1>
+    //   </section>
+    // </div>
   );
 }
